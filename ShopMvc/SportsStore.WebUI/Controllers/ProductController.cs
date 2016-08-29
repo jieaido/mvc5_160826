8ac5afa6d0423cc04a2484dd452479f4937df030
Cui-NotePC <jieaido@163.com>
2016-08-29 14:41:32 +0800

    修改默认路由和创建list页面

---
+++ ShopMvc/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+        public ViewResult List()
+        {
+            return View(productRepository.Products);
+        }
+    }
+}
