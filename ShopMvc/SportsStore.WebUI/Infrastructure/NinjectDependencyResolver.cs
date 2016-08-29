e46b03f5039f0dde824737bb76048a38ba8caf7f
Cui-NotePC <jieaido@163.com>
2016-08-29 10:09:38 +0800

    更新mock模仿绑定库

--- ShopMvc/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ ShopMvc/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -19,7 +22,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            
+           Mock<IProductRepository> mock=new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>()
+            {
+                new Product() {Name = "FootBall", Price = 25},
+                new Product() {Name = "suff board", Price = 225},
+                new Product() {Name = "running shoe", Price = 35}
+                });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
 
         public object GetService(Type serviceType)
