33bb184616f8a410f725255cd0fa3360f834bfb1
Jieaido <Jieaido@DESKTOP-PT2D06H>
2016-08-29 21:19:25 +0800

    手动输入页码分页

--- ShopMvc/SportsStore.WebUI/Controllers/ProductController.cs
+++ ShopMvc/SportsStore.WebUI/Controllers/ProductController.cs
@@ -10,16 +10,18 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository productRepository;
-
+        private int pagesize = 4;
         public ProductController(IProductRepository productRepository)
         {
             this.productRepository = productRepository;
         }
 
 
-        public ViewResult List()
+        public ViewResult List(int page=1)
         {
-            return View(productRepository.Products);
+            var result= productRepository.Products.OrderBy(p => p.ProductID).Skip((page - 1)*pagesize).Take(pagesize);
+
+            return View(result);
         }
     }
 }
