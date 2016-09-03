a527ffa6e3616037f342968119f7fd8b8052ab04
Jieaido <Jieaido@DESKTOP-PT2D06H>
2016-09-03 08:49:28 +0800

    布局页增加了当前购物数量和结账的按钮 但是按钮位置溢出了。先不解决

--- ShopMvc/SportsStore.WebUI/Controllers/CartController.cs
+++ ShopMvc/SportsStore.WebUI/Controllers/CartController.cs
@@ -57,6 +57,10 @@ namespace SportsStore.WebUI.Controllers
             return cart;
         }
 
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
         // GET: Cart
         
     }
